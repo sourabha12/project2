70c2663c3fc84a915061951e7c3eb243069123ed
sourabha <143331798+sourabha12@users.noreply.github.com>
2024-02-15 13:05:39 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+ import Navbar from "./components/Navbar"; 
+import About from "./components/About"; 
+import Skills from "./components/Skills"; 
+import Projects from "./components/Project"; 
+import Contact from "./components/Contact"; 
+import Footer from "./components/Footer"; 
+import Hero from "./components/Hero"; 
+  
+export default function App() { 
+    return ( 
+        <> 
+            <Navbar /> 
+          <About /> 
+            <Skills /> 
+            <Projects /> 
+            <Contact /> 
+            <Footer /> 
+              <Hero /> 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </> 
+    ); 
 }
-
-export default App;
